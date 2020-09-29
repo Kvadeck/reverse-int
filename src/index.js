936001aded8f5ea136e922f488cb3e307b423e46
kvadeck <kvadeck@yandex.ru>
2020-09-29 14:43:50 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let strArr = n.toString().split('').reverse();
+    const index = strArr.indexOf('-');
+    if (index > -1) { strArr.splice(index, 1) }
+    return strArr.join('')
 }
